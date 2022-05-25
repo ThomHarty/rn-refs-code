f05a9b7bb56e7fc3a1859c615bf15d104e31dd79
Thom Harty <tommyharty@192.168.1.104>
2022-05-25 18:16:19 +0100

    RN Refs Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import React, { ReactElement, useEffect, useRef, useState } from "react";
-import { SafeAreaView, TextInput } from "react-native";
+import React, {ReactElement, useEffect, useRef, useState} from 'react';
+import {SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
+
+interface Code {
+  0: string;
+  1: string;
+  2: string;
+  3: string;
+}
 
 export const App = (): ReactElement => {
   const inputRef1 = useRef<TextInput>(null);
@@ -7,29 +14,95 @@ export const App = (): ReactElement => {
   const inputRef3 = useRef<TextInput>(null);
   const inputRef4 = useRef<TextInput>(null);
 
-  const [code, setCode] = useState({0:'', 1:'', 2:'', 3: ''})
+  const [code, setCode] = useState<Code>({0: '', 1: '', 2: '', 3: ''});
 
   useEffect(() => {
-    if(code[3] !== '') {
-      console.log('submit form')
-    } else if(code[2] !== '') {
-      inputRef4.current?.focus()
-    } else if(code[1] !== '') {
-      inputRef3.current?.focus()
-    } else if(code[0] !== '') {
-      inputRef2.current?.focus()
+    if (code[3] !== '') {
+      console.log('submit form');
+    } else if (code[2] !== '') {
+      inputRef4.current?.focus();
+    } else if (code[1] !== '') {
+      inputRef3.current?.focus();
+    } else if (code[0] !== '') {
+      inputRef2.current?.focus();
     } else {
-      inputRef1.current?.focus()
+      inputRef1.current?.focus();
     }
-  }, [code])
+  }, [code]);
 
+  const handleBack = (input: number) => {
+    if (input === 4) {
+      inputRef3.current?.focus();
+    } else if (input === 3) {
+      inputRef2.current?.focus();
+    } else if (input === 2) {
+      inputRef1.current?.focus();
+    }
+  };
 
   return (
     <SafeAreaView>
-      <TextInput maxLength={1} ref={inputRef1} onChangeText={(value) => setCode({...code, 0: value})} style={{ margin: 5, backgroundColor: '#ebebeb' }} />
-      <TextInput maxLength={1} ref={inputRef2} onChangeText={(value) => setCode({...code, 1: value})} style={{ margin: 5, backgroundColor: '#ebebeb' }} />
-      <TextInput maxLength={1} ref={inputRef3} onChangeText={(value) => setCode({...code, 2: value})} style={{ margin: 5, backgroundColor: '#ebebeb' }} />
-      <TextInput maxLength={1} ref={inputRef4} onChangeText={(value) => setCode({...code, 3: value})} style={{ margin: 5, backgroundColor: '#ebebeb' }} />
+      <View style={styles.container}>
+        <TextInput
+          maxLength={1}
+          ref={inputRef1}
+          onChangeText={value => setCode({...code, 0: value})}
+          style={styles.input}
+          keyboardType='numeric'
+        />
+        <TextInput
+          maxLength={1}
+          ref={inputRef2}
+          onChangeText={value => setCode({...code, 1: value})}
+          style={styles.input}
+          keyboardType='numeric'
+          onKeyPress={({nativeEvent}) => {
+            nativeEvent.key === 'Backspace' && handleBack(2);
+          }}
+          editable={code[0] !== ''}
+        />
+        <TextInput
+          maxLength={1}
+          ref={inputRef3}
+          onChangeText={value => setCode({...code, 2: value})}
+          style={styles.input}
+          keyboardType='numeric'
+          onKeyPress={({nativeEvent}) => {
+            nativeEvent.key === 'Backspace' && handleBack(3);
+          }}
+          editable={code[1] !== ''}
+        />
+        <TextInput
+          maxLength={1}
+          ref={inputRef4}
+          onChangeText={value => setCode({...code, 3: value})}
+          style={styles.input}
+          keyboardType='numeric'
+          onKeyPress={({nativeEvent}) => {
+            nativeEvent.key === 'Backspace' && handleBack(4);
+          }}
+          editable={code[2] !== ''}
+        />
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    margin: 5,
+    backgroundColor: '#ebebeb',
+    width: 50,
+    height: 50,
+    padding: 10,
+    textAlign: 'center',
+    borderRadius: 5,
+  }
+})
